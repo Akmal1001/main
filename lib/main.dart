a76663fa64d57193dc23ffb69c2a5eacd5d191bc
Akmal <akmalagzamov95@gmail.com>
2023-07-05 11:13:13 +0500

    uzb flag

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
-import 'package:aplication/core/home_state.dart';
 import 'package:aplication/view/home_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(MultiBlocProvider(
-      providers: [BlocProvider(create: (context) => HomeCubit())],
-      child: MyApp()));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,37 +1,74 @@
-import 'package:aplication/core/home_state.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<HomeCubit, HomeState>(builder: (context, state) {
-      return Scaffold(
-        appBar: AppBar(title: const Text('App')),
-        body: Builder(
-          builder: (context) {
-            if (state is HomeLoadingState) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (state is HomeErrorState) {
-              return Center(child: Text(state.error));
-            } else if (state is HomeComplateState) {
-              return ListView.builder(
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(state.data[index].name.toString()),
-                    );
-                  },
-                  itemCount: state.data.length);
-            } else {
-              return const SizedBox();
-            }
-          },
-        ),
-      );
-    });
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 150),
+            height: 100,
+            width: double.infinity,
+            color: Colors.blue,
+            child: Row(
+              children: [
+                Icon(Icons.dark_mode,color: Colors.white,size: 60.0),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(Icons.star,color: Colors.white,size: 30.0),
+                         Icon(Icons.star,color: Colors.white,size: 30.0),
+                          Icon(Icons.star,color: Colors.white,size: 30.0)
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.star,color: Colors.white,size: 30.0),
+                         Icon(Icons.star,color: Colors.white,size: 30.0),
+                          Icon(Icons.star,color: Colors.white,size: 30.0),
+                          Icon(Icons.star,color: Colors.white,size: 30.0)
+                      ],
+                    ),
+                       Row(
+                      children: [
+                        Icon(Icons.star,color: Colors.white,size: 30.0),
+                         Icon(Icons.star,color: Colors.white,size: 30.0),
+                          Icon(Icons.star,color: Colors.white,size: 30.0),
+                          Icon(Icons.star,color: Colors.white,size: 30.0),
+                          Icon(Icons.star,color: Colors.white,size: 30.0)
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+          Container(
+            height: 10,
+            width: double.infinity,
+            color: Colors.red,
+          ),
+          SizedBox(
+            height: 100,
+
+          ),
+          Container(
+            height: 10,
+            width: double.infinity,
+            color: Colors.red,
+          ),
+             Container(
+            height: 100,
+            width: double.infinity,
+            color: Colors.green,
+          ),
+        ],
+      ),
+    );
   }
 }
